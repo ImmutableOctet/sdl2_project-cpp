b0b6d9196b79741ace7fa50a276941de0087e587
ImmutableOctet <sonickid8@live.com>
2016-02-27 16:54:50 -0700

    Initial cleanup of 'EBO.cpp'.

--- Game/game/graphics/EBO.cpp
+++ Game/game/graphics/EBO.cpp
@@ -41,7 +41,7 @@ namespace game
 		// Destructor(s):
 		elementBufferObject::~elementBufferObject()
 		{
-			// Nothing so far.
+			destroy();
 		}
 
 		// Operator overloads:
@@ -53,20 +53,14 @@ namespace game
 		}
 
 		// Methods:
-		bool elementBufferObject::init(const std::vector<std::vector<GLuint>>& indexDataArray, GLenum usage, bool destroyFirst)
+		bool elementBufferObject::init(const GLuint* indexDataArray, GLenum usage)
 		{
-			// Check if we should release existing handles first:
-			if (destroyFirst)
-			{
-				destroy(true);
-			}
-
-			return rawBufferUpload<GLuint, IBOHandle>(this->instances, indexDataArray, usage, GL_ELEMENT_ARRAY_BUFFER);
+			return rawBufferUpload<GLuint, IBOHandle>(this->instance, indexDataArray, usage, GL_ELEMENT_ARRAY_BUFFER);
 		}
 
-		void elementBufferObject::destroy(bool clear)
+		void elementBufferObject::destroy()
 		{
-			destroyBuffers(this->instances, clear);
+			destroyBuffers(1, &this->instance);
 
 			return;
 		}
