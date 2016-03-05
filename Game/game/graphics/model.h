4aa026669578f3748a0840f6c0aa7aea1de4a20a
ImmutableOctet <sonickid8@live.com>
2016-03-05 06:46:32 -0700

    Created 'model.h'.

---
+++ Game/game/graphics/model.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// Includes:
+#include "VAO.h"
+
+#include <vector>
+
+// Namespace(s):
+namespace game
+{
+	namespace graphics
+	{
+		// Classes:
+		class model
+		{
+			public:
+				// Fields:
+				//std::vector<vertexArrayObject> meshes;
+		};
+	}
+}
